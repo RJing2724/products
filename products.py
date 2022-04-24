34ea88657cb2d1448da900e6ccb6283e3b1a03b7
RJing2724 <roy.zhuangjingbo@yahoo.com>
2022-04-24 02:44:34 -0400

    version updated

--- products.py
+++ products.py
@@ -1,4 +1,4 @@
-# products
+# python3 products.py
 # 如何将多个清单放在一起
 
 products = []
@@ -14,7 +14,10 @@ while True:
 	p = [name, price]
 	products.append(p) #将p加入products
 	# 简写法: products.append([name, price])
-print(products)
+print(products)	# 输出大清单
 
 # 存取清单 
-products[0][0] # 大清单的第0格 + 小清单的第0格
+products[0][0]	# 大清单的第0格 + 小清单的第0格
+
+for p in products:
+	print('The price of', p[0], 'is $', p[1])	# 输出每个小清单
