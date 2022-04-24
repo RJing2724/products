f85e9d7d37027c2ab574db74abf5661f66200ed3
RJing2724 <roy.zhuangjingbo@yahoo.com>
2022-04-24 02:32:40 -0400

    first commit

---
+++ products.py
@@ -0,0 +1,20 @@
+# products
+# 如何将多个清单放在一起
+
+products = []
+
+while True:
+	name = input('Please enter product name.')
+	if name == 'q':
+		break
+	price = input('Please enter the price of product.')
+#	p = []	# 建立小清单
+#	p.append(name)	# 将两项分别加入p中
+#	p.append(price)
+	p = [name, price]
+	products.append(p) #将p加入products
+	# 简写法: products.append([name, price])
+print(products)
+
+# 存取清单 
+products[0][0] # 大清单的第0格 + 小清单的第0格
