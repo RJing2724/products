99dd5ab56077c5a226a07765ed9cd819f54a679c
RJing2724 <roy.zhuangjingbo@yahoo.com>
2022-04-25 01:05:16 -0400

    add read file

--- products.py
+++ products.py
@@ -1,9 +1,23 @@
 # python3 products.py
-# 如何将多个清单放在一起
-# 如何写入档案
+
 
 products = []
 
+# 4/25读取档案并切割
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f: # 将前几天创建的csv文件中的每一行每一行取名为line
+		if 'name, price' in line:
+			continue	# 跳过name和price，读取下一行
+		name, price = line.strip().split(',') # 将','当做切割的标志，记得要去掉(strip)换行符号'\n'
+		# 不用额外取函数，直接将清单列表[X, X]存入[name, price]
+		# print(s), split输出之后是清单格式[]
+		products.append([name, price])
+
+print(products)
+
+
+
+# 4/23如何将多个清单放在一起
 while True:
 	name = input('Please enter product name.')
 	if name == 'q':
@@ -32,12 +46,13 @@ for p in products:
 with open ('products.csv', 'w', encoding = 'utf-8') as f: # 也可以用txt等等
 								# 使用utf-8语言编码防止输入中文乱码, 
 								# 如果输出显示仍然有问题，需要在excel的data中对读取文件的输出编码进行选择
-	f.write('products, price \n')	# 加名称栏
+	f.write('name, price \n')	# 加名称栏
 
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n') # 每一项都是字串，写入进file
 		# ',' 将字串分开存入excel的表中, 记得'\n'必须要加上
 # with在loop结束时自动将档案关闭(python独有)
+
 
 
 
