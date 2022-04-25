cc2adc2be8fbb11710adca0f206c66abe6b1c2b5
RJing2724 <roy.zhuangjingbo@yahoo.com>
2022-04-25 02:33:10 -0400

    add check file exists

--- products.py
+++ products.py
@@ -1,23 +1,32 @@
 # python3 products.py
-
+# 记账本
 
 products = []
 
-# 4/25读取档案并切割
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f: # 将前几天创建的csv文件中的每一行每一行取名为line
-		if 'name, price' in line:
-			continue	# 跳过name和price，读取下一行
-		name, price = line.strip().split(',') # 将','当做切割的标志，记得要去掉(strip)换行符号'\n'
+# 4/25检查是否已有products.csv存在
+import os	# 载入operating system操作系统模组，例如windows，android，apple，linux
+			# 给这段程序码权限检查电脑文件
+if os.path.isfile('products.csv'):	# 查询路径的isfile功能，寻找file
+	print('The file already exists...')
+
+# 4/25文件存在的情况下读取档案并切割
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f: # 将前几天创建的csv文件中的每一行每一行取名为line
+			if 'name, price' in line:
+				continue	# 跳过name和price，读取下一行
+				name, price = line.strip().split(',') # 将','当做切割的标志，记得要去掉(strip)换行符号'\n'
 		# 不用额外取函数，直接将清单列表[X, X]存入[name, price]
 		# print(s), split输出之后是清单格式[]
-		products.append([name, price])
+			products.append([name, price])
+	print(products)
+else:
+	print('The file does not exists... Please enter the name of products and price.')
 
-print(products)
 
 
 
 # 4/23如何将多个清单放在一起
+# 让使用者输入
 while True:
 	name = input('Please enter product name.')
 	if name == 'q':
@@ -32,8 +41,9 @@ while True:
 print(products)	# 输出大清单
 
 # 存取清单 
-products[0][0]	# 大清单的第0格 + 小清单的第0格
+# products[0][0]	# 大清单的第0格 + 小清单的第0格
 
+# 输出购买记录
 for p in products:
 	print('The price of', p[0], 'is $', p[1])	# 输出每个小清单
 	# 假如p[0]是数字，则以str(p[0])写入print
@@ -43,6 +53,7 @@ for p in products:
 # 'abc' + '123' = 'abc123'
 # 'abc' * 3 = 'abcabcabc'
 
+# 将购买记录写成csv or txt文件
 with open ('products.csv', 'w', encoding = 'utf-8') as f: # 也可以用txt等等
 								# 使用utf-8语言编码防止输入中文乱码, 
 								# 如果输出显示仍然有问题，需要在excel的data中对读取文件的输出编码进行选择
